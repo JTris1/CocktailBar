31f78065ac49b897b4532e0708b2e3c57163aa82
JTris1 <johnathontrisler@gmail.com>
2022-12-02 18:38:01 -0500

    Added query for names only

--- api/routes/index.js
+++ api/routes/index.js
@@ -9,8 +9,14 @@ const Recipe = mongoose.model('recipes');
 // Get all recipes
 router.get('/', async function(req, res, next) {
   // req.query allows for passing in parameters for query supplied by query string
+  if(req.query["names"] === 'true') {
+    const namesOnly = await Recipe.find({},{name: 1});
+    res.json(namesOnly);
+  }
+  else {
   const recipes = await Recipe.find(req.query);
   res.json(recipes);
+  }
 });
 
 module.exports = router;
